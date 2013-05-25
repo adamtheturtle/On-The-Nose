b094b5a4585c55119c39c16dd803c2b1a0bf08db
Adam Dangoor <adamdangoor@gmail.com>
2013-05-25 08:34:26 +0100

    Now with images of Sarah

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ render = web.template.render('templates/')
 class index:
     def GET(self):
         directory = os.listdir('static/nose_images/')
-        nose_images = [file[:-4] for file in directory if file[-4:] == '.png']
+        nose_images = [file[:-5] for file in directory if file[-5:] == '.jpeg']
         noses = [(nose.split(',')[0], nose.split(',')[1])
                  for nose in nose_images if ',' in nose]
 
